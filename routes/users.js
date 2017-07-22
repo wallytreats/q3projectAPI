0cc93a7a622862a5c315aa2e790a1e591ffef2b8
Brennen Bull <brennenbull@gmail.com>
2017-07-21 19:17:28 -0600

    route test passing

--- routes/users.js
+++ routes/users.js
@@ -5,39 +5,50 @@ const saltRounds = 8;
 const bcrypt = require('bcrypt');
 
 router.get('/login', (req, res, next)=>{
-
+  console.log('login');
+  res.sendStatus(200);
 
 });
 
 router.get('/create', (req, res, next)=>{
-
-
+  console.log('create');
+  res.sendStatus(200);
 });
 
 
 router.get('/:id', (req, res, next)=>{
+  console.log(('id'));
   let id = req.params.id;
-
+  console.log(id);
+  res.sendStatus(200);
 });
 
 router.post('/:id', (req, res, next)=>{
   let id = req.params.id;
   let body = req.body;
+  console.log(id);
+  res.sendStatus(200);
 });
 
 router.post('/:id', (req, res, next)=>{
   let id = req.params.id;
   let body = req.body;
+  console.log(id);
+  res.sendStatus(200);
 });
 
 router.patch('/:id', (req, res, next)=>{
   let id = req.params.id;
   let body = req.body;
+  console.log(id);
+  res.sendStatus(200);
 });
 
 router.delete('/:id', (req, res, next)=>{
   let id = req.params.id;
   let body = req.body;
+  console.log(id);
+  res.sendStatus(200);
 });
 
 module.exports = router;
